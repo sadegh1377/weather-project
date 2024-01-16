77148643df2fbaa0c7eb274ef2fb76a42f0e007f
sadeg1377 <sadeghhadipour1@gmail.com>
2024-01-16 12:03:54 +0330

    add axios and vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import axios from "axios"
+
+const base = axios.create({
+    baseURL: "http://api.weatherapi.com/v1/",
+    params: {
+        key: "88f9beb316ad4c07aa683609240501"
+    }
+});
+
+Vue.prototype.$http = base;
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
