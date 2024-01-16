916d14a352ccde2414a904976e73d8f141e9a92b
sadeg1377 <sadeghhadipour1@gmail.com>
2024-01-16 12:08:39 +0330

    remove unnecessary line of code and add font family

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <v-app>
+        <div class="app main">
+            <router-view/>
+        </div>
+    </v-app>
 </template>
 
+<script>
+
+export default {
+    name: 'App',
+
+    data: () => ({
+        //
+    }),
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+
+.app {
+    font-family: 'Inter', sans-serif;
+    height: 100%;
+    background-color: white;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.v-application--wrap {
+    max-width: unset !important;
 }
 </style>
