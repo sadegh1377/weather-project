a11c84b7405a03c848c2ab5803906f77ea432c1c
sadeg1377 <sadeghhadipour1@gmail.com>
2024-01-17 14:55:05 +0330

    fix mdi icon

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from "axios"
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.use(vuetify, {
+    iconfont: 'mdi'
+})
 
 const base = axios.create({
     baseURL: "http://api.weatherapi.com/v1/",
